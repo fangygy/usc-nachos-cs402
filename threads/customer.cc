8d3a26975e155104a7bc6949ad704cf185206773
cnc224 <cnc224@gmail.com>
2011-09-15 22:54:07 +0000

    add group

--- threads/customer.cc
+++ threads/customer.cc
@@ -11,15 +11,28 @@ Customer::Customer(int cId, int gId, int gSize, bool ticketBuyer) {
 }
 Customer::~Customer() {}
 
+void Customer::waitGroup() {
+    for (int i = 1;i < groupSize; ++i) {
+        sGroup[groupId]->P();
+    }
+}
 void Customer::action() {
     if (getIsTicketBuyer()) {
         // ticket buyer
         buyTickets();
-        buyFood();
+        waitGroup();
+        if (countFood()) {
+            buyFood();
+        }
+        waitGroup();
         checkTickets();
+        waitGroup();
+        arrangeSeats();
     } else {
         // regular customer
-        
+        waitTickets();
+        waitFood();
+        waitSeats();
     }
 }
 bool Customer::getIsTicketBuyer() {
@@ -97,12 +110,19 @@ void Customer::buyTickets() {
         clerk->condition[1]->Signal(clerk->lock);
     } else {
         clerk->subWaitingSize();
+        // for race condition of waiting size
         lBuyTickets->Release();
         clerk->lock->Release();
         buyTickets();
         return;
     }
     clerk->lock->Release();
+    // tell group to go
+    lGroup[groupId]->Acquire();
+    cGroup[groupId]->Broadcast(lGroup[groupId]);
+    // monitor variables
+    groupTicket[groupId] = true;
+    lGroup[groupId]->Release();
 }
 
 void Customer::buyFood() {
@@ -113,46 +133,60 @@ void Customer::buyFood() {
 
     ConcessionClerk *clerk = cc[lineIndex];
     clerk->condition[0]->Wait(lBuyFood);
-    lBuyFood->Release();
     // interact with ConcessionClerk 
     clerk->lock->Acquire();
     if (!clerk->getIsBreak()) {
-        // TODO: tell clerk popcorn sum
-        int popcorn = countFood("popcorn");
-        clerk->condition[1]->Signal(clerk->lock);
-
-        // wait for acknowledgement 
-        clerk->condition[1]->Wait(clerk->lock);
-
-        // TODO: tell clerk soda sum
-        int soda = countFood("soda");
-        clerk->condition[1]->Signal(clerk->lock);
-
-        // wait for amount 
-        clerk->condition[1]->Wait(clerk->lock);
+        lBuyFood->Release();
+        // TODO: tell clerk popcorn sum and soda sum
+        for (int i = 0;i < 2; ++i) {
+            clerk->setFood(i, groupFoodSum[groupId][i]);
+            clerk->condition[1]->Signal(clerk->lock);
+            // wait for acknowledgement and amount
+            clerk->condition[1]->Wait(clerk->lock);
+        }
         // TODO: give money
         double amount = clerk->getAmount();
         clerk->setPayment(amount);
         clerk->condition[1]->Signal(clerk->lock);
+    } else {
+        clerk->subWaitingSize();
+        // for race condition of waiting size
+        lBuyFood->Release();
+        clerk->lock->Release();
+        buyFood();
+        return;
     }
     clerk->lock->Release();
 
 }
 
-int Customer::countFood(char * food) {
+bool Customer::countFood() {
     if (getIsTicketBuyer()) {
-        // TODO
-        return 1;
+        lGroup[groupId]->Acquire();
+        // if self want food
+        for (int i = 0;i < 2; ++i) {
+            groupFoodSum[groupId][i] += answerForFood(i);
+        }
+        // ask group mate if they want food
+        cGroupFood[groupId]->Broadcast(lGroup[groupId]);
+        lGroup[groupId]->Release();
+        //sGroupFood[groupId]->V();
+        // wait for all the customer answer for food
+        for (int i = 1;i < groupSize; ++i) {
+            sGroupFood[groupId]->P();
+        }
+        if (!groupFoodSum[groupId][0] && !groupFoodSum[groupId][1]) {
+            groupFood[groupId] = true;
+            return false;
+        }
+        return true;
     }
-    return 0;
+    return false;
 }
-
-bool Customer::askForFood(char * food) {
-    return true;
-}
-
-bool Customer::answerForFood(char * food) {
-    return true;
+// rand for food
+int Customer::answerForFood(int food) {
+    int choice = rand()%4;
+    return (rand()%4) > 0;
 }
 
 void Customer::checkTickets() {
@@ -163,17 +197,27 @@ void Customer::checkTickets() {
 
     TicketTaker *clerk = tt[lineIndex];
     clerk->condition[0]->Wait(lCheckTickets);
-    lCheckTickets->Release();	
     // interact with TicketTaker 
     clerk->lock->Acquire();
     if (!clerk->getIsBreak()) {
+        lCheckTickets->Release();    
+        // give ticket receipt 
+        clerk->setTicketSum(groupTicketSum[groupId]);
         clerk->condition[1]->Signal(clerk->lock);
-        // TODO:  
 
         clerk->condition[1]->Wait(clerk->lock);
-        // TODO: give money
+        
+        // get into room
         clerk->condition[1]->Signal(clerk->lock);
+    } else {
+        clerk->subWaitingSize();
+        // for race condition of waiting size
+        lCheckTickets->Release();
+        clerk->lock->Release();
+        checkTickets();
+        return;
     }
+
     clerk->lock->Release();
 }
 
@@ -182,34 +226,86 @@ void Customer::goBathroom() {
 }
 void Customer::leaveTheater() {
 }
+void Customer::arrangeSeats() {
+    ;
+}
+void Customer::waitTickets() {
+    lGroup[groupId]->Acquire();
+    // if ticketbuyer had broadcast, do not wait
+    if (!groupTicket[groupId]) {
+        cGroup[groupId]->Wait(lGroup[groupId]);
+    }
+    // after get ticket, semaphore
+    sGroup[groupId]->V();
+    lGroup[groupId]->Release();
+}
+// regular customer wait for buyFood
+void Customer::waitFood() {
+    lGroup[groupId]->Acquire();
+    // wait for ticketbuyer to ask for if need food
+    if (!groupAskForFood[groupId]) {
+        cGroupFood[groupId]->Wait(lGroup[groupId]);
+    }
+    for (int i = 0;i < 2; ++i) {
+        groupFoodSum[groupId][i] += answerForFood(i);
+    }
+    sGroupFood[groupId]->V();
+    // if ticketbuyer had broadcast, do not wait
+    if (!groupFood[groupId]) {
+        cGroup[groupId]->Wait(lGroup[groupId]);
+    }
+    // after get food, semaphore
+    sGroup[groupId]->V();
+    lGroup[groupId]->Release();
+}
+void Customer::waitSeats() {
+    lGroup[groupId]->Acquire();
+    // if ticketbuyer had broadcast, do not wait
+    if (!groupFood[groupId]) {
+        cGroup[groupId]->Wait(lGroup[groupId]);
+    }
+
+    // take seat
+    lStartMovie->Acquire();
+    // get seat number from ticketbuyer
+    //seatState[] = ture;
+    //seatNumber = 
+    sGroup[groupId]->V();
+    lStartMovie->Release();
+
+    // after get seat, semaphore
+    sGroup[groupId]->V();
+    lGroup[groupId]->Release();
+}
+
 void Customer::printTCStatus(){
 
-	//Print the number of waiting customer in each clerk's line
+    //Print the number of waiting customer in each clerk's line
 
-	printf("TC State:");
-	 for (int i = 0;i < MAX_TC; ++i) {
-		 TicketClerk *clerk = tc[i];
-		 if(clerk!=NULL){ // if clerk have not been create, don't print
-			 printf("%d,",clerk->getIsBusy());
-		}else{
-                    printf("NA,"); 
-                 }
-	}
-	printf("\n");
+    printf("TC State:");
+    for (int i = 0;i < MAX_TC; ++i) {
+        TicketClerk *clerk = tc[i];
+        if(clerk!=NULL){ // if clerk have not been create, don't print
+            printf("%d,",clerk->getIsBusy());
+        }else{
+            printf("NA,"); 
+        }
+    }
+    printf("\n");
 
 
-	//Print the number of waiting customer in each clerk's line
+    //Print the number of waiting customer in each clerk's line
 
-	 printf("TC WaitLine(#):");
+    printf("TC WaitLine(#):");
 
-	 for (int i = 0;i < MAX_TC; ++i) {
-		 TicketClerk *clerk = tc[i];
-		 if(clerk!=NULL){ // if clerk have not been create, don't print
-			 printf("%d,",clerk->getWaitingSize());
-		}else{
-                    printf("NA,"); 
-                 }
-	 }
-	 printf("\n");
+    for (int i = 0;i < MAX_TC; ++i) {
+        TicketClerk *clerk = tc[i];
+        if(clerk!=NULL){ // if clerk have not been create, don't print
+            printf("%d,",clerk->getWaitingSize());
+        }else{
+            printf("NA,"); 
+        }
+    }
+    printf("\n");
 
 }
--- threads/movietheater.cc
+++ threads/movietheater.cc
@@ -9,8 +9,36 @@ MovieTechnician * mt[MAX_MT];
 Manager * mr[MAX_MR];
 
 Lock *lBuyTickets = new Lock("Lock_BuyTickets_line");
-Lock *lBuyFood = new Lock("Lock_BuyFood_line");;
-Lock *lCheckTickets = new Lock("Lock_CheckTickets_line");;
+Lock *lBuyFood = new Lock("Lock_BuyFood_line");
+Lock *lCheckTickets = new Lock("Lock_CheckTickets_line");
+Lock *lStartMovie = new Lock("Lock_StartMovie");
+Semaphore *sGroup[MAX_GROUP];
+Condition *cGroup[MAX_GROUP];
+Condition *cGroupFood[MAX_GROUP];
+Lock *lGroup[MAX_GROUP];
+int groupFoodSum[MAX_GROUP][2];
+int groupTicketSum[MAX_GROUP];
+Semaphore *sGroupFood[MAX_GROUP];
+bool groupTicket[MAX_GROUP];
+bool groupAskForFood[MAX_GROUP];
+bool groupFood[MAX_GROUP];
+bool groupSeat[MAX_GROUP];
 
+void init() {
+    memset(groupFoodSum, 0, sizeof(groupFoodSum));
+    memset(groupTicketSum, 0, sizeof(groupTicketSum));
+    memset(groupTicket, 0, sizeof(groupTicket));
+    memset(groupAskForFood, 0, sizeof(groupAskForFood));
+    memset(groupFood, 0, sizeof(groupFood));
+    memset(groupSeat, 0, sizeof(groupSeat));
 
+    int i;
+    for (i = 0;i < MAX_GROUP; ++i) {
+        sGroup[i] = new Semaphore("Semaphore_Group", 0);
+        sGroupFood[i] = new Semaphore("Semaphore_GroupFood", 0);
+        cGroup[i] = new Condition("Condition_Group");
+        cGroupFood[i] = new Condition("Condition_GroupFood");
+        lGroup[i] = new Lock("Lock_Group");
+    }
+}
 
