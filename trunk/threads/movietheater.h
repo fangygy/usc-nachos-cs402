f5b2340f75a5947be8dbd6bf562b311e3f1b93ac
cnc224@gmail.com <cnc224@gmail.com@1f28ecc5-28e2-e816-d5a3-2c04efaf6078>
2011-09-15 23:57:46 +0000

    add comments

--- trunk/threads/movietheater.h
+++ trunk/threads/movietheater.h
@@ -20,21 +20,21 @@
 #define RATE_SODA 0.75  // chance to buy Soda
 #define RATE_BATHROOM 0.25  // chance to go bathroom
 
-extern Lock *lBuyTickets;
-extern Lock *lBuyFood;
-extern Lock *lCheckTickets;
-extern Lock *lStartMovie;
-extern Semaphore *sGroup[MAX_GROUP];
-extern Condition *cGroup[MAX_GROUP];
-extern Condition *cGroupFood[MAX_GROUP];
-extern Lock *lGroup[MAX_GROUP];
-extern int groupFoodSum[MAX_GROUP][2];
-extern int groupTicketSum[MAX_GROUP];
-extern Semaphore *sGroupFood[MAX_GROUP];
-extern bool groupTicket[MAX_GROUP];
-extern bool groupAskForFood[MAX_GROUP];
-extern bool groupFood[MAX_GROUP];
-extern bool groupSeat[MAX_GROUP];
+extern Lock *lBuyTickets;  //Lock to get in line of buyTickets
+extern Lock *lBuyFood;  //Lock to get in line of buyFood
+extern Lock *lCheckTickets;  //Lock to get in line of checkTickets
+extern Lock *lStartMovie;  //Lock to get seats and startMovie
+extern Semaphore *sGroup[MAX_GROUP];  //semaphore to make group act together
+extern Condition *cGroup[MAX_GROUP];  //Condition to make group act together
+extern Condition *cGroupFood[MAX_GROUP];  //Condition for ticketbuyer to ask for if need food
+extern Lock *lGroup[MAX_GROUP];  //Lock for group to act mutex
+extern int groupFoodSum[MAX_GROUP][2];  // order of group for food
+extern int groupTicketSum[MAX_GROUP];  // order of group for tickets
+extern Semaphore *sGroupFood[MAX_GROUP];  //semaphore to make sure everyone choose if need food
+extern bool groupTicket[MAX_GROUP];  // monitor variable for if buyTickets done
+extern bool groupAskForFood[MAX_GROUP];  // monitor variable for if ask for food
+extern bool groupFood[MAX_GROUP];  // monitor variable for if buyFood done
+extern bool groupSeat[MAX_GROUP];  // monitor variable for if get seats
 
 extern bool seatState[25];
 
@@ -44,12 +44,16 @@ class Employee {
     bool isBreak;
     int id;
     char employeeType[MAX_VAR];
+    // customer in waiting line
     int waitingSize;
   protected:
     void setIsBusy(bool busy);
     void setIsBreak(bool isBreak);
   public:
+    // for action between with customer and employee
     Lock *lock;
+    // c0 is for customer in waiting line
+    // c1 is action between customer and employee
     Condition *condition[2];
     bool getIsBusy();
     bool getIsBreak();
@@ -58,7 +62,9 @@ class Employee {
     ~Employee();
     char * getEmployeeType();
     int getWaitingSize();
+    // waitingSize add 1
     void addWaitingSize();
+    // waitingSize sub 1
     void subWaitingSize();
 };
 
@@ -68,14 +74,15 @@ class Customer {
     int groupId;
     bool isTicketBuyer;
     int groupSize;   //Only head customers need this value
-// how to find groupmate
-    Customer * customers;   //Only head customers need this value
+    // how to find groupmate
+    //    Customer * customers;   //Only head customers need this value
     void printTCStatus();
     int seatNumber;
   public:
     // customerId, groupId, isTicketBuyer
     Customer(int cId, int gId, int gSize, bool ticketBuyer);
     ~Customer();
+    // customer action for ticketBuyer and regular Customer
     void action();
     bool getIsTicketBuyer();
     // for ticketBuyer to buy tickets from ticketClerk
@@ -93,9 +100,13 @@ class Customer {
     // arrange seats
     void arrangeSeats();
     // for regular custormer
+    // wait for buy tickets done
     void waitTickets();
+    // wait for buy food done
     void waitFood();
+    // wait for take seats done
     void waitSeats();
+    // wait for group act together
     void waitGroup();
     void setSeatNumber(int seatNum);
 
@@ -157,6 +168,7 @@ extern ConcessionClerk *cc[MAX_CC];
 
 class TicketTaker : public Employee{
   private:
+    // tickets sum one group has
     int ticketSum; 
   public:
     TicketTaker(int ttId);
