30e0f69a08b4efea601788a9992ec2055f8da599
cnc224 <cnc224@gmail.com>
2011-09-16 22:48:25 +0000

    Makefile common

--- threads/problem2.cc
+++ threads/problem2.cc
@@ -15,7 +15,7 @@ void test1();
 void tc_new(int i) {
     //printf("start tc_new\n");
     //printf("%s: new TicketClerk %d\n", currentThread->getName(), i);
-    tc[i] = new TicketClerk(i);
+//    tc[i] = new TicketClerk(i);
     tc[i]->sellTickets();
     //printf("%s: TicketClerk %d created\n", currentThread->getName(), i);
 }
@@ -24,7 +24,7 @@ void cr_tb_new(int arg) {
     //printf("start cr_tb_new\n");
     CustomerData *customerData = (CustomerData *) arg; 
     //printf("%s: new Customer TicketBuyer %d\n", currentThread->getName(), customerData->customerNumber);
-    cr[customerData->customerNumber] = new Customer(customerData->customerNumber, customerData->groupNumber, customerData->groupSize, true);
+    //cr[customerData->customerNumber] = new Customer(customerData->customerNumber, customerData->groupNumber, customerData->groupSize, true);
     cr[customerData->customerNumber]->action();
     //printf("%s: Customer TicketBuyer %d created\n", currentThread->getName(), customerData->customerNumber);
 }
@@ -41,7 +41,7 @@ void cr_new(int arg) {
 void cc_new(int i) {
     //printf("start cc_new\n");
     //printf("%s: new ConcessionClerk %d\n", currentThread->getName(), i);
-    cc[i] = new ConcessionClerk(i);
+    //cc[i] = new ConcessionClerk(i);
     cc[i]->sellFood();
     //printf("%s: ConcessionClerk %d created\n", currentThread->getName(), i);
 }
@@ -49,7 +49,7 @@ void cc_new(int i) {
 void tt_new(int i) {
    // printf("start tt_new\n");
     //printf("%s: new TicketTaker %d\n", currentThread->getName(), i);
-    tt[i] = new TicketTaker(i);
+    //tt[i] = new TicketTaker(i);
     tt[i]->checkTickets();
    // printf("%s: TicketTaker %d created\n", currentThread->getName(), i);
 }
@@ -78,6 +78,8 @@ void test1() {
     for (int i = 0;i < MAX_TC; ++i) {
         name = new char[MAX_VAR];
         sprintf(name, "Thread_TicketClerk_%d", i); 
+        // ? in thread or hear ?
+        tc[i] = new TicketClerk(i);
         t = new Thread(name);
         t->Fork((VoidFunctionPtr)tc_new, i);
     }
@@ -94,6 +96,8 @@ void test1() {
             customerData->customerNumber = nextCustomerNumber++;
             customerData->groupNumber = i;
             customerData->groupSize = groupSize;
+            // ? in thread or here
+            cr[customerData->customerNumber] = new Customer(customerData->customerNumber, customerData->groupNumber, customerData->groupSize, false);
             t = new Thread(name);
             t->Fork((VoidFunctionPtr)cr_new, int(customerData));
         }
@@ -103,6 +107,8 @@ void test1() {
         customerData->customerNumber = nextCustomerNumber++;
         customerData->groupNumber = i;
         customerData->groupSize = groupSize;
+        // ? in thread or here
+        cr[customerData->customerNumber] = new Customer(customerData->customerNumber, customerData->groupNumber, customerData->groupSize, true);
         t = new Thread(name);
         t->Fork((VoidFunctionPtr)cr_tb_new, int(customerData));
     }
@@ -112,6 +118,8 @@ void test1() {
     for (int i = 0;i < MAX_CC; ++i) {
         name = new char[MAX_VAR];
         sprintf(name, "Thread_ConcessionClerk_%d", i);
+        // ? in thread or here
+        cc[i] = new ConcessionClerk(i);
         t = new Thread(name);
         t->Fork((VoidFunctionPtr)cc_new, i);
     }
@@ -120,19 +128,24 @@ void test1() {
     for (int i = 0;i < MAX_TT; ++i) {
         name = new char[MAX_VAR];
         sprintf(name, "Thread_TicketTaker_%d", i);
+        // ? in thread or here
+        tt[i] = new TicketTaker(i);
         t = new Thread(name);
         t->Fork((VoidFunctionPtr)tt_new, i);
     }
-     printf("Number of TicketTakers = [%d]\n",MAX_TT);
+    printf("Number of TicketTakers = [%d]\n",MAX_TT);
 
     // create MovieTechnician 
     for (int i = 0;i < MAX_MT; ++i) {
         name = new char[MAX_VAR];
         sprintf(name, "Thread_MovieTechnician_%d", i);
+        // ? in thread or here
+        mt[i] = new MovieTechnician(i);
         t = new Thread(name);
         t->Fork((VoidFunctionPtr)mt_new, i);
     }
      
+    mr[0]->work();
 }
 void Problem2() {
     init();
